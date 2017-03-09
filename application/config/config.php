5b1a3d895046ed84917f1b4d2beee315151d3b47
CerberusOne <aingaran2003@gmail.com>
2017-03-09 14:10:05 -0800

    created SQL dump closes #17
    Signed-off-by: CerberusOne <aingaran2003@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -383,7 +383,7 @@ $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = 'ci_sessions';     // this should be a writeable folder on your system
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = FALSE; 
 
 /*
 |--------------------------------------------------------------------------
